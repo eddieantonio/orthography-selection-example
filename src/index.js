847005c75a1ba12e9ace396cd5c055fc2727d265
Eddie Antonio Santos <easantos@ualberta.ca>
2020-02-18 21:09:22 -0700

    Change orth works?

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-export function changeOrth () {
+function changeOrth (which) {
   let elements = document.querySelectorAll('[data-orth]')
+  console.assert(['Cans', 'Latn', 'LatnXMacron'].includes(which))
+
+  let attr = `orth${which}`;
   for (let el of elements) {
-    el.innerText = el.dataset.orthCans;
+    let newText = el.dataset[attr];
+    if (newText) {
+      el.innerText = newText;
+    }
   }
 }
 
-setTimeout(changeOrth, 1)
+setTimeout(() => changeOrth('LatnXMacron'), 1000)
+setTimeout(() => changeOrth('Cans'), 2000)
+setTimeout(() => changeOrth('Latn'), 3000)
